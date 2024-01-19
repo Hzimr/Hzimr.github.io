b5de58203769bbf1a14a586b2e18c99c5a39ec5d
Hítalo Rodrigues <hitalo005@gmail.com>
2024-01-19 12:25:25 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | H-Project',
+    default: 'Hítalo Project'
+  },
+  description: "Portfólio do Hítalo Rodrigues",
+  keywords: ['Hítalo', 'Project'],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br" className="dark antialiased">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
