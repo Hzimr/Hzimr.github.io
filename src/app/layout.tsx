6f04ef107f5020e045086681b7dbfc76fc344721
Hítalo Rodrigues <hitalo005@gmail.com>
2024-01-19 13:09:11 -0300

    chore: prettier setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: {
     template: '%s | H-Project',
-    default: 'Hítalo Project'
+    default: 'Hítalo Project',
   },
-  description: "Portfólio do Hítalo Rodrigues",
+  description: 'Portfólio do Hítalo Rodrigues',
   keywords: ['Hítalo', 'Project'],
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="pt-br" className="dark antialiased">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
