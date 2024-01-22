0a6fccb47123e41de338ef505cf357bedf43a640
Hítalo Rodrigues <hitalo005@gmail.com>
2024-01-22 12:35:14 -0300

    styles: Included new colors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,26 @@ const config: Config = {
   darkMode: 'class',
   content: ['./src/**/*tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          dark: '#212429',
+          dark2: '#1F2227',
+          darkLine: '#1D2025',
+          darkContent: '#22252A',
+        },
+        second: {
+          redcolor1: '#E80D54',
+          redcolor2: '#D9124F',
+          redDarkColor: '#621732',
+        },
+        textColor: {
+          lightColor1: '#F7F9FA',
+          lightColor2: '#C2CBDC',
+          lightColor3: '##BAC2CC',
+        },
+      },
+    },
   },
   plugins: [],
 }
