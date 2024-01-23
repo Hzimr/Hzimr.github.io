55f0fe37720f6e1edbe0d474f163d0c9d32fa7a1
Hítalo Rodrigues <hitalo005@gmail.com>
2024-01-22 23:05:26 -0300

    feat: initial commit in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import { MyImage } from '@/components/myImage/myImage'
+
 export default function Home() {
-  return <></>
+  return (
+    <div className="mt-16 flex items-center justify-center p-12">
+      <div className="flex items-center justify-between gap-10 p-24 lg:p-12">
+        <div>
+          <section className="flex flex-col gap-5">
+            <h2>Welcome to my Page</h2>
+            <h1 className="text-4xl">
+              Hi, I{`'`}m{' '}
+              <strong className="text-second-redcolor1 ">
+                Hítalo Rodrigues
+              </strong>{' '}
+              <br /> a Web Developer
+            </h1>
+            <p>Technologies I use the most:</p>
+          </section>
+        </div>
+        <MyImage />
+      </div>
+    </div>
+  )
 }
