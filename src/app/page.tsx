f32ddd7b56566766291e733892d86b7f4a2fe61d
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-16 12:10:42 -0300

    feat: created section Talk and Skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { MyImage } from '@/components/myImage/myImage'
 export default function Home() {
   return (
     <div className="mt-16 flex items-center justify-center p-12">
-      <div className="flex items-center justify-between gap-10 p-24 lg:p-12">
+      <div className="flex h-[600px] items-center justify-between gap-10 p-24 lg:p-12">
         <div>
-          <section className="flex flex-col gap-5">
+          <article className="flex flex-col gap-5">
             <h2>Welcome to my Page</h2>
             <h1 className="text-4xl">
               Hi, I{`'`}m{' '}
@@ -15,7 +15,17 @@ export default function Home() {
               <br /> a Web Developer
             </h1>
             <p>Technologies I use the most:</p>
-          </section>
+            <div className="grid grid-cols-2">
+              <section className="flex flex-col">
+                <h1>TALK TO ME:</h1>
+                <div>oi</div>
+              </section>
+              <section className="flex flex-col">
+                <h1>BEST SKILL ON:</h1>
+                <div>oi</div>
+              </section>
+            </div>
+          </article>
         </div>
         <MyImage />
       </div>
