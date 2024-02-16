daac18c521723bc3fdc504dfe251b6e406267d03
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-16 16:40:48 -0300

    feat: create TypewriterEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
 import { MyImage } from '@/components/myImage/myImage'
+import { TypeMsg } from '@/utils/typeMsg'
 
 export default function Home() {
   return (
     <div className="mt-16 flex items-center justify-center p-12">
       <div className="flex h-[600px] items-center justify-between gap-10 p-24 lg:p-12">
         <div>
-          <article className="flex flex-col gap-5">
+          <article className="flex w-[420px] flex-col gap-5">
             <h2>Welcome to my Page</h2>
-            <h1 className="text-4xl">
-              Hi, I{`'`}m{' '}
-              <strong className="text-second-redcolor1 ">
-                Hítalo Rodrigues
-              </strong>{' '}
-              <br /> a Web Developer
-            </h1>
+            <TypeMsg />
             <p>Technologies I use the most:</p>
             <div className="grid grid-cols-2">
               <section className="flex flex-col">
---
+++ src/utils/typeMsg.tsx
@@ -0,0 +1,53 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+
+interface TypingEffectProps {
+  text: string
+  delay: number
+}
+
+const TypingEffect = ({ text, delay }: TypingEffectProps) => {
+  const [displayText, setDisplayText] = useState('')
+  const [displayTextStrong, setDisplayTextStrong] = useState('')
+  const [displayTextContinues, setDisplayTextContinues] = useState('')
+
+  useEffect(() => {
+    let currentIndex = 0
+
+    const interval = setInterval(() => {
+      if (currentIndex < 8) {
+        setDisplayText(text.substring(0, currentIndex))
+        currentIndex++
+      } else if (currentIndex > 7 && currentIndex < 25) {
+        setDisplayTextStrong(text.substring(8, currentIndex))
+        currentIndex++
+      } else {
+        setDisplayTextContinues(text.substring(24, currentIndex))
+        currentIndex++
+      }
+
+      if (currentIndex > text.length) {
+        clearInterval(interval)
+      }
+    }, delay)
+
+    return () => clearInterval(interval)
+  }, [text, delay])
+
+  return (
+    <h1 className="after:container-[''] text-4xl after:relative after:animate-ping after:border-r-4 after:border-second-redcolor1">
+      {displayText}{' '}
+      <strong className="text-second-redcolor1 ">{displayTextStrong}</strong>{' '}
+      {displayTextContinues}
+    </h1>
+  )
+}
+
+export function TypeMsg() {
+  return (
+    <TypingEffect
+      text="Hi, I'm Hítalo Rodrigues a Web Developer"
+      delay={100} // Adjust the delay to control the typing speed
+    />
+  )
+}
