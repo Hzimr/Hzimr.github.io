97c650edd383adf2b8d580e689ee28fa6bd9a259
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-17 20:50:39 -0300

    chore: test this next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// eslint-disable-next-line prefer-const
+let envImageUnoptimize = process.env.NODE_ENV === 'production'
+
+const nextConfig = {
+  output: process.env.NODE_ENV !== 'production' ? undefined : 'export',
+  images: {
+    unoptimized: envImageUnoptimize,
+  },
+}
 
 export default nextConfig
