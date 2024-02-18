adca02952a944be3c944e71fbc6ec96adbcef22b
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-17 21:14:23 -0300

    feat: config next

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'export',
+  output: process.env.NODE_ENV !== 'production' ? undefined : 'export',
 }
 
 export default nextConfig
