0dcd239ea4e9291f0adc840abfe04b95f063ad66
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-17 21:04:53 -0300

    changed: pnpm to npm

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
-// eslint-disable-next-line prefer-const
-let envImageUnoptimize = process.env.NODE_ENV === 'production'
 
-const nextConfig = {
-  output: process.env.NODE_ENV !== 'production' ? undefined : 'export',
-  images: {
-    unoptimized: envImageUnoptimize,
-  },
-}
+const nextConfig = {}
 
 export default nextConfig
