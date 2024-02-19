716f9857891ad9921e150a09af9d11e64a89e613
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-19 13:01:19 -0300

    feat: created backbutton

---
+++ src/app/aboutus/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { BackButton } from '@/components/back-button'
+
+export function AboutUs() {
+  return (
+    <div className="relative flex flex-1 items-center justify-center bg-slate-400 p-8">
+      <BackButton navigate="/" />
+    </div>
+  )
+}
---
+++ src/components/back-button.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { IoArrowBackCircleOutline } from 'react-icons/io5'
+import { useRouter } from 'next/navigation'
+
+interface BtnProps {
+  navigate: string
+}
+
+export function BackButton({ navigate }: BtnProps) {
+  const router = useRouter()
+  const handleNavigate = () => {
+    router.push(navigate)
+  }
+  return (
+    <button
+      className="absolute left-3 top-2 flex cursor-pointer items-center justify-center gap-2 border-none p-2 text-sm font-medium"
+      onClick={handleNavigate}
+    >
+      <IoArrowBackCircleOutline />
+      Voltar
+    </button>
+  )
+}
