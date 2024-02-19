2358e630057c255744fd9bb48582dae8dd0b6a9d
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-19 20:09:31 -0300

    feat: created certificateItems

---
+++ src/data/certificateItems.ts
@@ -0,0 +1,22 @@
+import { Certificate } from './types/certificates'
+import certReact from '../assets/certificateReact.png'
+import certNode from '../assets/certificateReact.png'
+
+export const itemsCertificates: Certificate[] = [
+  {
+    id: crypto.randomUUID(),
+    title: 'Certificate React',
+    date: new Date('2024-02-10'),
+    category: 'string',
+    image: certReact,
+    description: 'NLW Expert Trilha React front-end',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Certificate Node',
+    date: new Date('2024-02-12'),
+    category: 'string',
+    image: certNode,
+    description: 'NLW Expert Trilha Node back-end',
+  },
+]
