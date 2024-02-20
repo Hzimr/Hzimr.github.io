a9f1081af1ad37fa2ff17400bbbf3f94f3227706
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-19 21:14:55 -0300

    refactor: changed type string to StaticImageData

--- src/data/types/certificates.ts
+++ src/data/types/certificates.ts
@@ -1,9 +1,11 @@
+import { StaticImageData } from 'next/image'
+
 export interface Certificate {
   id: string
   title: string
   date: Date
   category: string
-  image: string
+  image: StaticImageData
   description: string
   featured?: boolean
 }
