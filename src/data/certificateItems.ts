f24d341304e0b02a98f458fe27b444fd18f86603
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-21 14:35:38 -0300

    feat: added English and RemoteWork certification

--- src/data/certificateItems.ts
+++ src/data/certificateItems.ts
@@ -1,22 +1,40 @@
 import { Certificate } from './types/certificates'
-import certReact from '../assets/certificateReact.png'
-import certNode from '../assets/certificateNode.png'
+import certReact from '../assets/certificates/certificateReact.png'
+import certNode from '../assets/certificates/certificateNode.png'
+import certEnglish from '../assets/certificates/certificateInglês.png'
+import certRemoteWork from '../assets/certificates/certificateRemoteWork.png'
 
 export const itemsCertificates: Certificate[] = [
   {
     id: crypto.randomUUID(),
+    title: 'Certificate Node',
+    date: new Date('2024-02-12'),
+    category: 'hardskills',
+    image: certNode,
+    description: 'NLW Expert Trilha Node back-end',
+  },
+  {
+    id: crypto.randomUUID(),
     title: 'Certificate React',
     date: new Date('2024-02-10'),
-    category: 'string',
+    category: 'hardskills',
     image: certReact,
     description: 'NLW Expert Trilha React front-end',
   },
   {
     id: crypto.randomUUID(),
-    title: 'Certificate Node',
-    date: new Date('2024-02-12'),
-    category: 'string',
-    image: certNode,
-    description: 'NLW Expert Trilha Node back-end',
+    title: 'Certificate Remote Work',
+    date: new Date('2023-04-16'),
+    category: 'softskills',
+    image: certRemoteWork,
+    description: 'Remote Work Professional Certification',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Certificate English',
+    date: new Date('2022-12-05'),
+    category: 'softskills',
+    image: certEnglish,
+    description: 'Basic English Certification',
   },
 ]
