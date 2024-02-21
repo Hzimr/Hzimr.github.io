cfd3d5da4c71bc2d84bfbf9347dd38e9ba6918e9
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-21 20:01:26 -0300

    refactor: remove header in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '../components/theme/themeProvider'
-import { Header } from '../components/header/header'
 import { Sidebar } from '../components/sidebar/sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -27,9 +26,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class">
           <div className="min-h-screen bg-zinc-300 dark:bg-primary-dark lg:grid lg:grid-cols-app">
             <Sidebar />
-
             <main className="max-w-[100vw] px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pb-12 lg:pt-8">
-              <Header />
               {children}
             </main>
           </div>
