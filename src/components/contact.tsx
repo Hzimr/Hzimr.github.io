1d8ca573cc3e8ecae12bdedf0f7ad1fd57870791
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-23 20:01:37 -0300

    added p in section

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -6,11 +6,11 @@ export function Contact() {
     <div className="mt-6 flex h-24 flex-col items-center justify-between rounded-2xl bg-slate-200 p-3 text-xl text-black dark:bg-primary-darkContent dark:text-slate-100 md:h-16 md:w-[40rem] md:flex-row md:p-2">
       <section className="flex items-center gap-2">
         <MdOutlineEmail />
-        hitalorodriguesdev@gmail.com
+        <p>hitalorodriguesdev@gmail.com</p>
       </section>
       <section className="flex items-center gap-2">
         <FaPhoneVolume />
-        88 99687-0729
+        <p>88 99687-0729</p>
       </section>
     </div>
   )
