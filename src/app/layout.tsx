41b95623990cbeb4b492c4deceb8962be12f51c7
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-23 20:32:12 -0300

    feat: added variable font inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 import { ThemeProvider } from '../components/theme/themeProvider'
 import { Sidebar } from '../components/sidebar/sidebar'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
 export const metadata: Metadata = {
   title: {
@@ -21,9 +21,17 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-br" className="dark antialiased">
-      <body className={inter.className}>
-        <ThemeProvider attribute="class">
+    <html
+      lang="pt-br"
+      className={`${inter.variable} inter.variable dark antialiased`}
+    >
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           <div className="min-h-screen bg-zinc-300 dark:bg-primary-dark lg:grid lg:grid-cols-app">
             <Sidebar />
             <main className="max-w-[100vw] px-4 pb-12 pt-24 lg:col-start-2 lg:px-2 lg:pb-12 lg:pt-8">
