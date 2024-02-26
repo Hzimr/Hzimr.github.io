4e4964c8fdf90b84b72de08e8898c08dbfc79d0e
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-26 11:40:26 -0300

    feat: created page githubinfos struct my infos

--- src/app/githubInfos/page.tsx
+++ src/app/githubInfos/page.tsx
@@ -1,4 +1,8 @@
 import { BackButton } from '@/components/back-button'
+import Image from 'next/image'
+import { Suspense } from 'react'
+import { FaLocationDot, FaPersonWalking } from 'react-icons/fa6'
+import { MdPeopleOutline } from 'react-icons/md'
 
 export default async function Githubinfos() {
   await new Promise((resolve) => setTimeout(resolve, 300))
@@ -7,10 +11,44 @@ export default async function Githubinfos() {
   const user = await response.json()
 
   return (
-    <div className="relative mt-7 flex flex-1 flex-col items-center justify-center bg-slate-100 dark:bg-primary-darkContent">
-      <h1 className="text-2xl md:text-4xl">Githubinfos in construction...</h1>
-      <BackButton navigate="/" />
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-    </div>
+    <Suspense>
+      <div className="relative flex flex-col items-center justify-center">
+        <h1 className="animate-pulse text-2xl md:text-4xl">
+          Githubinfos in construction right now
+        </h1>
+        <BackButton navigate="/" />
+        <div className="relative mt-7 flex flex-1 flex-row justify-center bg-slate-100 dark:bg-primary-darkContent">
+          <div>
+            {' '}
+            <Image
+              className="rounded-full"
+              width={260}
+              height={260}
+              src={`${user.html_url}` + '.png'}
+              alt=""
+            />
+            <h1>{user.name}</h1>
+            <h2>{user.login}</h2>
+            <h3>{user.bio}</h3>
+            <div className="flex flex-col">
+              <section className="flex items-center gap-1">
+                <MdPeopleOutline />
+                <p>{user.followers}</p>
+                <span>followers</span>
+              </section>
+              <section className="flex items-center gap-1">
+                <FaPersonWalking />
+                <p>{user.following}</p>
+                <span>following</span>
+              </section>
+            </div>
+          </div>
+          <div>
+            {' '}
+            <pre>{JSON.stringify(user, null, 2)}</pre>
+          </div>
+        </div>
+      </div>
+    </Suspense>
   )
 }
