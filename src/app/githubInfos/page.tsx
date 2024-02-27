80704e0138ee50c5209875d3e35e105829868606
Hítalo Rodrigues <hitalo005@gmail.com>
2024-02-27 00:17:16 -0300

    feat: created repos gitinfo

--- src/app/githubInfos/page.tsx
+++ src/app/githubInfos/page.tsx
@@ -2,13 +2,35 @@ import { BackButton } from '@/components/back-button'
 import Image from 'next/image'
 import { Suspense } from 'react'
 import { FaLocationDot, FaPersonWalking } from 'react-icons/fa6'
-import { MdPeopleOutline } from 'react-icons/md'
+import {
+  MdOutlineCreateNewFolder,
+  MdOutlineUpdate,
+  MdPeopleOutline,
+} from 'react-icons/md'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
+interface Repo {
+  id: string
+  name: string
+  description?: string
+  updated_at: string
+  created_at: string
+  html_url: string
+}
 
 export default async function Githubinfos() {
-  await new Promise((resolve) => setTimeout(resolve, 300))
+  await new Promise((resolve) => setTimeout(resolve, 1300))
 
   const response = await fetch('https://api.github.com/users/hzimr')
   const user = await response.json()
+
+  const responseRepo = await fetch('https://api.github.com/users/Hzimr/repos')
+  const repo = await responseRepo.json()
+
+  const sortedRepos: Repo[] = repo.sort((a: Repo, b: Repo) => {
+    return b.updated_at.localeCompare(a.updated_at)
+  })
 
   return (
     <Suspense>
@@ -51,7 +73,38 @@ export default async function Githubinfos() {
           </div>
           <div>
             {' '}
-            <pre>{JSON.stringify(user, null, 2)}</pre>
+            <div className="flex flex-col text-xl">
+              {sortedRepos?.map((repo) => {
+                return (
+                  <div
+                    className="flex flex-col gap-2 border-b border-b-slate-50 p-3"
+                    key={repo.id}
+                  >
+                    <h1 className="text-2xl text-blue-500">{repo.name}</h1>
+                    <p>{repo?.description}</p>
+                    <section>
+                      <p className="fle-col flex items-center gap-2">
+                        <MdOutlineUpdate />
+                        {formatDistanceToNow(repo.updated_at, {
+                          locale: ptBR,
+                          addSuffix: true,
+                        })}
+                      </p>
+                      <p className="fle-col flex items-center gap-2">
+                        <MdOutlineCreateNewFolder />
+                        {formatDistanceToNow(repo.created_at, {
+                          locale: ptBR,
+                          addSuffix: true,
+                        })}
+                      </p>
+                    </section>
+                    <a href={repo.html_url} target="_blank">
+                      Link do projeto
+                    </a>
+                  </div>
+                )
+              })}
+            </div>
           </div>
         </div>
       </div>
