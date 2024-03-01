b6cc3b75dd0a7b1ba8b4915e489be62bcd216e78
Hítalo Rodrigues <hitalo005@gmail.com>
2024-03-01 17:08:25 -0300

    feat: added Logo and Header1

--- src/components/new-components/header.tsx
+++ src/components/new-components/header.tsx
@@ -4,6 +4,8 @@ import { useSelectedLayoutSegment } from 'next/navigation'
 import Link from 'next/link'
 import useScroll from '@/hooks/use-scroll'
 import { cn } from '@/utils/utils-cn'
+import { Logo } from '../header/logo'
+import { Header1 } from '../header/header'
 
 export function Header() {
   const scrolled = useScroll(5)
@@ -14,10 +16,9 @@ export function Header() {
       className={cn(
         `sticky inset-x-0 top-0 z-30 w-full border-b border-gray-500 transition-all dark:border-gray-200`,
         {
-          'backgrop-blur-lg border-b border-gray-500 bg-white/75 dark:border-gray-200':
+          'backgrop-blur-lg border-b border-gray-500 dark:border-gray-200 dark:bg-primary-darkContent/75':
             scrolled,
-          'border-b border-gray-500 bg-white dark:border-gray-200':
-            selectedLayout,
+          'border-b border-gray-500 dark:border-gray-200': selectedLayout,
         },
       )}
     >
@@ -27,14 +28,13 @@ export function Header() {
             href="/"
             className="flex flex-row items-center justify-center space-x-3 md:hidden"
           >
-            <span className="h-7 w-7 rounded-lg bg-zinc-600 dark:bg-zinc-300" />
-            <span className="flex text-xl font-bold">Logo</span>
+            <span className="flex text-xl font-bold">
+              <Logo />
+            </span>
           </Link>
         </div>
         <div className="hidden md:block">
-          <div className="rounderd-full flex h-8 w-8 items-center justify-center bg-zinc-300 text-center">
-            <span className="text-sm font-semibold">Hz</span>
-          </div>
+          <Header1 />
         </div>
       </div>
     </div>
