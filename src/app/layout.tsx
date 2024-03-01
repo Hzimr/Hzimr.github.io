b5998c5edc17dd30c1daff17716e806dd5e3109c
Hítalo Rodrigues <hitalo005@gmail.com>
2024-03-01 17:12:02 -0300

    refactor: created new structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,11 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '../components/theme/themeProvider'
-// import { Sidebar } from '../components/sidebar/sidebar'
-import { Newsidebar } from '@/components/sidebar/new-sidebar'
 import { Header } from '@/components/new-components/header'
 import { HeaderMobile } from '@/components/new-components/header-mobile'
+import { SideNav } from '@/components/new-components/side-nav'
+import { PageWrapper } from '@/components/new-components/page-wrapper'
+import { MarginWidthWrapper } from '@/components/new-components/margin-width-wrapper'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
@@ -30,13 +31,14 @@ export default function RootLayout({
     >
       <body>
         <ThemeProvider attribute="class">
-          <div className="min-h-screen bg-zinc-300 dark:bg-primary-dark lg:grid lg:grid-cols-app">
-            {/* <Sidebar /> */}
-            <Header />
-            <HeaderMobile />
-            <Newsidebar />
-            <main className="max-w-[100vw] px-4 pb-12 pt-24 lg:col-start-2 lg:px-2 lg:pb-12 lg:pt-8">
-              {children}
+          <div className="flex min-h-screen bg-zinc-300 dark:bg-primary-dark">
+            <SideNav />
+            <main className="flex-1">
+              <MarginWidthWrapper>
+                <Header />
+                <HeaderMobile />
+                <PageWrapper>{children}</PageWrapper>
+              </MarginWidthWrapper>
             </main>
           </div>
         </ThemeProvider>
