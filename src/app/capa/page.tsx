933a8117a7ebcd7587590fa97fe81e30beaab339
Hítalo Rodrigues <hitalo005@gmail.com>
2024-03-17 17:58:36 -0300

    chore: capa

---
+++ src/app/capa/page.tsx
@@ -0,0 +1,19 @@
+// import { FaReact } from 'react-icons/fa'
+
+export default function CapaPage() {
+  return (
+    <div className="flex flex-1 justify-center">
+      <a className="bg-gradient-to-r from-slate-500 via-indigo-900 to-textColor-lightColor2 bg-clip-text text-4xl text-transparent after:animate-ping after:border-r-4 after:border-indigo-400 dark:after:border-indigo-700">
+        Essa página é um Easter egg da capa do linkedin
+      </a>
+    </div>
+  )
+  //   return (
+  //     <div className="flex h-full w-full flex-1 flex-col items-center justify-center bg-[#23272F]">
+  //       <FaReact className="size-28 text-[#149DCA]" />
+  //       <h1 className="bg-gradient-to-r from-slate-500 via-indigo-900 to-textColor-lightColor2 bg-clip-text text-4xl text-transparent after:animate-ping after:border-r-4 after:border-indigo-400 dark:after:border-indigo-700">
+  //         hello, I&lsquo;m <code>&lt;/dev&gt; </code>
+  //       </h1>
+  //     </div>
+  //   )
+}
