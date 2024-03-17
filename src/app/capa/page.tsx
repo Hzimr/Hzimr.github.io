89853b2b49c6c8d7fef1d81a9acb98829c3ee80e
Hítalo Rodrigues <hitalo005@gmail.com>
2024-03-17 17:59:22 -0300

    fix: added href

--- src/app/capa/page.tsx
+++ src/app/capa/page.tsx
@@ -3,7 +3,10 @@
 export default function CapaPage() {
   return (
     <div className="flex flex-1 justify-center">
-      <a className="bg-gradient-to-r from-slate-500 via-indigo-900 to-textColor-lightColor2 bg-clip-text text-4xl text-transparent after:animate-ping after:border-r-4 after:border-indigo-400 dark:after:border-indigo-700">
+      <a
+        href="/"
+        className="bg-gradient-to-r from-slate-500 via-indigo-900 to-textColor-lightColor2 bg-clip-text text-4xl text-transparent after:animate-ping after:border-r-4 after:border-indigo-400 dark:after:border-indigo-700"
+      >
         Essa página é um Easter egg da capa do linkedin
       </a>
     </div>
