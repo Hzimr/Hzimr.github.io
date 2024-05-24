669174faff7af974bad620862775cb7c89ee8eb5
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-24 16:23:52 -0300

    refactor: props className in DefaultLayout added

--- src/components/theme/defaultPageLayout.tsx
+++ src/components/theme/defaultPageLayout.tsx
@@ -2,11 +2,14 @@ import { ReactNode } from 'react'
 
 interface DefaultLayoutProps {
   children: ReactNode
+  className?: string
 }
 
-export function DefaultLayout({ children }: DefaultLayoutProps) {
+export function DefaultLayout({ children, className }: DefaultLayoutProps) {
   return (
-    <div className="mx-auto my-0 flex min-h-screen w-full max-w-[1200px] flex-col items-center justify-center px-4 py-3">
+    <div
+      className={`${className} mx-auto my-0 flex min-h-screen w-full max-w-[1200px] flex-col px-4 py-3`}
+    >
       {children}
     </div>
   )
