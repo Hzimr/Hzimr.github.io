81fa453ade854d26faf032e174f818e93db14f57
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-24 17:18:22 -0300

    feat: estrutura inicial da main da home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,49 @@
 import { DefaultLayout } from '@/components/theme/defaultPageLayout'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa6'
+import { LuNewspaper } from 'react-icons/lu'
 
 export default function Home() {
   return (
     <div className="flex w-screen flex-col items-center justify-center">
       <main className="w-screen">
         <div className="flex justify-center bg-gradient-to-br from-[#ff0083] to-[#ae0036]">
-          <DefaultLayout>
+          <DefaultLayout className="items-center justify-center">
             <div className="">
               <p className="text-xl text-white">
                 OI, EU SOU O HÍTALO RODRIGUES
               </p>
 
-              <a
-                href="/pdf.pdf"
-                target="_blank"
-                rel="noreferrer"
-                className="text-white"
-              >
-                PDF
-              </a>
-
               <div>
-                <h1 className="text-[10rem] font-bold text-white">REACT</h1>
-                <section></section>
+                <section className="flex flex-row items-center gap-7">
+                  <h1 className="text-[10rem] font-bold text-white">REACT</h1>{' '}
+                  <a
+                    href="/pdf.pdf"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="pt-4 text-white"
+                    title="Github"
+                  >
+                    <FaGithub size={64} />
+                  </a>
+                  <a
+                    href="/pdf.pdf"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="pt-4 text-white"
+                    title="LinkedIn"
+                  >
+                    <FaLinkedinIn size={64} />
+                  </a>
+                  <a
+                    href="/pdf.pdf"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="pt-4 text-white"
+                    title="Currículo"
+                  >
+                    <LuNewspaper size={64} />
+                  </a>
+                </section>
               </div>
               <h1 className="text-[10rem] font-bold text-white">DEVELOPER</h1>
             </div>
@@ -32,9 +53,11 @@ export default function Home() {
             </p>
           </DefaultLayout>
         </div>
-        <div className="flex w-screen justify-center bg-gradient-to-r from-[#161129] to-[#060F23]">
+        <div className="flex w-screen bg-gradient-to-r from-[#161129] to-[#060F23]">
           <DefaultLayout>
-            <div className=""></div>
+            <div className="text-4xl text-white">
+              Projetos que já desenvolvi
+            </div>
           </DefaultLayout>
         </div>
       </main>
