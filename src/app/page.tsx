523b6ab26c493d85f97bed5ca9e5723898492ef1
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-24 15:04:06 -0300

    styles: new style home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,19 @@ export default function Home() {
   return (
     <div className="flex w-screen flex-col items-center justify-center">
       <main className="w-screen">
-        <div className="flex justify-center bg-purple-400">
+        <div className="flex justify-center bg-gradient-to-br from-[#ff0083] to-[#ae0036]">
           <DefaultLayout>
             <div className="">
-              <p className="text-xl">OI, EU SOU O HÍTALO RODRIGUES</p>
+              <p className="text-xl text-white">
+                OI, EU SOU O HÍTALO RODRIGUES
+              </p>
 
-              <a href="/pdf.pdf" target="_blank" rel="noreferrer">
+              <a
+                href="/pdf.pdf"
+                target="_blank"
+                rel="noreferrer"
+                className="text-white"
+              >
                 PDF
               </a>
 
@@ -19,13 +26,13 @@ export default function Home() {
               </div>
               <h1 className="text-[10rem] font-bold text-white">DEVELOPER</h1>
             </div>
-            <p>
+            <p className="text-white">
               TENHO 28 ANOS SOU DESENVOLVEDOR FRONTEND COM MAIS DE 1 ANOS DE
               EXPERIÊNCIA TRABALHANDO COMO DESENVOLDEDOR{' '}
             </p>
           </DefaultLayout>
         </div>
-        <div className="flex w-screen justify-center bg-blue-700">
+        <div className="flex w-screen justify-center bg-gradient-to-r from-[#161129] to-[#060F23]">
           <DefaultLayout>
             <div className=""></div>
           </DefaultLayout>
